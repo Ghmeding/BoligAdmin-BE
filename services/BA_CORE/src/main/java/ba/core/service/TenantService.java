01320557493f1a93dfcfe516a7c96573b45aba9d
Gustav Meding <gmeding@deloitte.dk>
2026-02-18 19:26:21 +0100

    feat: add rabbitmq config

--- services/BA_CORE/src/main/java/ba/core/service/TenantService.java
+++ services/BA_CORE/src/main/java/ba/core/service/TenantService.java
@@ -7,14 +7,17 @@ import ba.core.models.TenantEntity;
 import ba.core.repository.PropertyRepository;
 import ba.core.repository.TenantRepository;
 import jakarta.transaction.Transactional;
+import org.springframework.cloud.stream.function.StreamBridge;
 import org.springframework.stereotype.Service;
 
 @Service
 public class TenantService {
+    private final StreamBridge streamBridge;
     private final TenantRepository tenantRepository;
     private final PropertyRepository propertyRepository;
 
-    public TenantService(TenantRepository tenantRepository, PropertyRepository propertyRepository){
+    public TenantService(StreamBridge streamBridge, TenantRepository tenantRepository, PropertyRepository propertyRepository){
+        this.streamBridge = streamBridge;
         this.tenantRepository = tenantRepository;
         this.propertyRepository = propertyRepository;
     }
@@ -24,6 +27,7 @@ public class TenantService {
         PropertyEntity propertyEntity = propertyRepository.getReferenceById(createTenantDTO.getPropertyId());
 
         TenantEntity tenantEntity = TenantMapper.convertToEntity(createTenantDTO, propertyEntity);
+        streamBridge.send("tenantCreatedProducer-out-0", createTenantDTO);
 
         return tenantRepository.save(tenantEntity).getId();
     }
